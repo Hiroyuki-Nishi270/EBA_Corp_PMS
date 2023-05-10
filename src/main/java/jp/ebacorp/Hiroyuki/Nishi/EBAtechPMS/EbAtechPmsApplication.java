8a1a06cebbdb2d29cd1e3f9c74fd14ff9d1457c8
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-10 18:48:49 +0900

    初回

---
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/EbAtechPmsApplication.java
@@ -0,0 +1,13 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbAtechPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbAtechPmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/EbAtechPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbAtechPmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
