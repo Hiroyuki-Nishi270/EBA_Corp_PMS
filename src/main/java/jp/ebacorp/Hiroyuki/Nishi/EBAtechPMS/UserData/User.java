d3e5d5953511e8ee251bd2720749a9e63375dd9e
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-12 05:15:35 +0900

    Userエンティティを修正

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/User.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/User.java
@@ -1,14 +1,17 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Table(name = "Users")
+@Getter
+@Setter
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String name;
