fa79de3009aa9f40e1d4b4b5db269b609a8fbb0c
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-12 02:54:11 +0900

    indexを暫定作成

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/WebSecurityConfig.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/home","index").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
