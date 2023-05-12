ff86a411c55ed436a47e256c8718cb530ad0b8a5
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-12 20:33:24 +0900

    index用のコントローラーを作成（途中）

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/MainController.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/MainController.java
@@ -11,10 +11,8 @@ public class MainController {
 
     @GetMapping({"/","/index","index"})
     String indexController(Model model){
-        //List<TaskItem> taskItems = dao.findAll();
-        //model.addAttribute("taskList", taskItems);
-
-        model.addAttribute("title","インデックス");
+        model.addAttribute("title","EBAtechPMS");
+        model.addAttribute("h1","EBAtech Project Management System");
 
         System.out.println("index is called");
         return "index";
