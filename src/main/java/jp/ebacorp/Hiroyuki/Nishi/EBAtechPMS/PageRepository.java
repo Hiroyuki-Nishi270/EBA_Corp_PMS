1d82e8b0ca69735fc2938acfe833448ccb33adc8
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-13 20:08:20 +0900

    PageRepository作成

---
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/PageRepository.java
@@ -0,0 +1,7 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PageRepository  extends CrudRepository<Page,Long> {
+
+}
