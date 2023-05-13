9b56cc17f53b683e6e1b67dbc6a364addab5d125
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-13 22:12:32 +0900

    registerへのリンクをindex.htmlに追加

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/PageData/PageRepository.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/PageData/PageRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PageRepository extends CrudRepository<Page,Long> {
 
+    public static String getAllMappingString(String controllerName){
+
+        return "";
+    }
 }
