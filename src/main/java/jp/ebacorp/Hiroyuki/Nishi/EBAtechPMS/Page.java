50c38955285921275bcb804503d0e6d456c26b7f
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-13 19:43:05 +0900

    Pageクラス作成

---
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/Page.java
@@ -0,0 +1,21 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Pages")
+@Data
+public class Page {
+
+    @Id
+    private Integer id;
+
+    private String mappingString;
+
+    private String viewName;
+
+    private String pageTitle;
+}
