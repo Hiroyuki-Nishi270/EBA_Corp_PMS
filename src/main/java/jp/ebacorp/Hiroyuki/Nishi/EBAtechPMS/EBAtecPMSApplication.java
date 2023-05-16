4103f33df909bc46be8d05ace8530e8ce09cf59d
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-17 08:20:13 +0900

    不要な記述を削除

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/EBAtecPMSApplication.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/EBAtecPMSApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EBAtechPMSApplication {
+public class EBAtecPMSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EBAtechPMSApplication.class, args);
+		SpringApplication.run(EBAtecPMSApplication.class, args);
 	}
 
 }
