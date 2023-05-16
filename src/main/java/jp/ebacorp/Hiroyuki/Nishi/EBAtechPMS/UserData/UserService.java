84817042940e0d385a07ac8c74803f4f52371daf
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-17 08:13:58 +0900

    不要な記述を削除

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/UserService.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/UserService.java
@@ -1,12 +1,9 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData;
 
-
-import jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData.User;
-import jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-
 
 //User(会員)の情報登録のためのServiceクラス
 //削除とパスワード変更用のメソッドも後々必要。
@@ -16,8 +13,7 @@ public class UserService {
     UserRepository userRepository;
 
     //暗号化処理のインスタンス生成
-    //PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     // ユーザー登録用API
     public void createUser(String name, String email, String rawPassword) {
@@ -26,7 +22,7 @@ public class UserService {
         user.setName(name);
         user.setEmail(email);
         // パスワードはハッシュ化する
-        //user.setPassword(passwordEncoder.encode(rawPassword));
+        user.setPassword(passwordEncoder.encode(rawPassword));
         user.setPassword(rawPassword);
         userRepository.save(user);
     }
