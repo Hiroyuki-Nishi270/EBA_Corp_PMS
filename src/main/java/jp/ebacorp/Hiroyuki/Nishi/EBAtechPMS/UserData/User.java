5e07d48c1934caeb84fc38b8675757798c4d1ade
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-17 08:25:22 +0900

    GenerationTypeを修正

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/User.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/User.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
