af6d25ea3fbd0f28dfc36ded70d022ade2ade762
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-17 20:24:41 +0900

    RepositoryにメソッドexistByemailを追加

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/UserRepository.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/UserData/UserRepository.java
@@ -3,7 +3,11 @@ package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 //User(会員)のリポジトリ。DBのテーブルをJavaのインターフェースにしたようなもの
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByemail(String email);
+
 }
