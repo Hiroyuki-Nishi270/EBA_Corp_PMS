c6bf0b0c39f8050b97629d5b282f3653a7924ebf
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-18 23:08:43 +0900

    UserDataパッケージを不要にした。UserDetailsなどを使用

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/MainController.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/MainController.java
@@ -1,19 +1,31 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
 
-import jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS.UserData.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.sql.DataSource;
+
 
 @Controller
 public class MainController {
 
+    //@Autowired
+    //UserService userService;
+
     @Autowired
-    UserService userService;
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    DataSource dataSource;
 
     @GetMapping("/")
     String indexController(Model model){
@@ -41,19 +53,26 @@ public class MainController {
             @RequestParam("password1") String password1,
             @RequestParam("password2") String password2
             ){
-
         if(password1.equals(password2)){
-            if(userService.createUser(username,email,password1)){
-                model.addAttribute("result","会員登録に成功しました！");
-                model.addAttribute("title","EBAtecPMS");
-                return "index";
+            try {
+                UserDetails s = userDetailsService.loadUserByUsername(username);
 
-            }else {
                 model.addAttribute("title","会員登録 | EBAtecPMS");
                 model.addAttribute("result","このメールアドレスは既に登録されております");
                 model.addAttribute("username",username);
                 model.addAttribute("email",email);
                 return "register";
+
+            }catch (UsernameNotFoundException e){
+                UserDetails user = User.builder().username(username).password(password1).roles("USER").build();
+                JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+                users.createUser(user);
+                System.out.println("test");
+
+                model.addAttribute("result","会員登録に成功しました！");
+                model.addAttribute("title","EBAtecPMS");
+                return "index";
             }
         }else{
             model.addAttribute("title","会員登録 | EBAtecPMS");
--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/WebSecurityConfig.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/WebSecurityConfig.java
@@ -2,10 +2,18 @@ package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -39,4 +47,15 @@ public class WebSecurityConfig {
 
         return new InMemoryUserDetailsManager(user);
     }**/
+    @Bean
+    public UserDetailsService userDetailsService (DataSource datasource) {
+        return new JdbcUserDetailsManager(datasource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
