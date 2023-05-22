e4205e0a45cdf31807c2b7560f216dfb7c796682
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-22 20:08:48 +0900

    ユーザー登録時のバリデーション(BEサイド)を作成

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
@@ -1,20 +1,19 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class registerForm {
 
-    //@NotNull(message = "名前が入っておりません")
-    @Length(min = 1, max = 32, message = "usernameを１文字以上３２文字以内に納めてください")
+    @Pattern(regexp = "[0-9a-zA-Z]{1,32}", message = "User nameには１文字以上３２文字以内の英数字のみを使用してください")
     private String username;
 
-    //@NotNull(message = "パスワードが空欄です")
-    @Length(min = 1, max = 32, message = "password1を１文字以上３２文字以内に納めてください")
+    @Pattern(regexp = "^[ -~]+${1,32}", message = "passwordには１文字以上３２文字以内の英数字と記号のみを使用してください")
     private String password1;
 
-    //@NotNull(message = "パスワードが空欄です")
-    @Length(min = 1, max = 32, message = "passoword2を１文字以上３２文字以内に納めてください")
     private String password2;
 }
