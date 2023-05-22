f28d1af3393abaa226c067932066abead3becc9e
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-22 18:26:40 +0900

    registerFormを修正

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
@@ -1,21 +1,20 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 public class registerForm {
 
-    @NotNull(message = "名前が入っておりません")
-    @Length(min = 1, max = 32, message = "名前の長さが不適切です。３２文字以内に納めてください")
+    //@NotNull(message = "名前が入っておりません")
+    @Length(min = 1, max = 32, message = "１文字以上３２文字以内に納めてください")
     private String username;
 
-    @NotNull(message = "パスワードが空欄です")
-    @Length(min = 1, max = 32, message = "３２文字以内に納めてください")
+    //@NotNull(message = "パスワードが空欄です")
+    @Length(min = 1, max = 32, message = "１文字以上３２文字以内に納めてください")
     private String password1;
 
-    @NotNull(message = "パスワードが空欄です")
-    @Length(min = 1, max = 32, message = "３２文字以内に納めてください")
+    //@NotNull(message = "パスワードが空欄です")
+    @Length(min = 1, max = 32, message = "１文字以上３２文字以内に納めてください")
     private String password2;
 }
