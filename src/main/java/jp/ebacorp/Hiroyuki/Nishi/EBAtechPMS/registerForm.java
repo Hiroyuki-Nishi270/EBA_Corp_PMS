8d11f92dca512ec2e6e6000f902e5f749da23ecf
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-22 18:17:33 +0900

    registerFormを追加(registerのバリデーションクラス)

---
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
@@ -0,0 +1,18 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class registerForm {
+
+    @NotNull(message = "名前が入っておりません")
+    @Length(min = 1, max = 32, message = "名前の長さが不適切です。３２文字以内に納めてください")
+    private String username;
+
+    @NotNull(message = "パスワードが空欄です")
+    @Length(min = 1, max = 32, message = "３２文字以内に納めてください")
+    private String password;
+
+}
