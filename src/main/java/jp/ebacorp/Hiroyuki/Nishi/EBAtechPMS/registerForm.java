50db0b763743d354f475b89ce807eabb026bc707
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-24 18:56:06 +0900

    正規表現を変更。FE側のバリデーション(HTML)を作成

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
@@ -9,10 +9,10 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class registerForm {
 
-    @Pattern(regexp = "[0-9a-zA-Z]{1,32}", message = "User nameには１文字以上３２文字以内の英数字のみを使用してください")
+    @Pattern(regexp = "[0-9a-zA-Z]{1,32}", message = "ユーザー名には１文字以上３２文字以内の英数字のみを使用してください")
     private String username;
 
-    @Pattern(regexp = "^[ -~]+${1,32}", message = "passwordには１文字以上３２文字以内の英数字と記号のみを使用してください")
+    @Pattern(regexp = "(?=.*[A-Z])(?=.*[.?/-])(?=.*[a-z])[a-zA-Z0-9.?/-]{8,24}", message = "passwordには８文字以上２４文字以内の英数字と記号のみを使用してください")
     private String password1;
 
     private String password2;
