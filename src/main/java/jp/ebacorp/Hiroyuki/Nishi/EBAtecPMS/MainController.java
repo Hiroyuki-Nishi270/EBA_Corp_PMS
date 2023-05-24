3ec8a27d5369365b2321f5411e27676a21f5688c
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-25 00:27:54 +0900

    不要なコードを削除

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtecPMS/MainController.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtecPMS/MainController.java
@@ -42,19 +42,12 @@ public class MainController {
     @GetMapping("/")
     String indexController(Model model){
         model.addAttribute("title","EBAtecPMS");
-
-        System.out.println("indexController is called");
-
         return "index";
     }
 
     @GetMapping("register")
     String registerController(Model model){
-
         model.addAttribute("title","会員登録 | EBAtecPMS");
-
-        System.out.println("registerController is called");
-
         return "register";
     }
 
@@ -82,31 +75,6 @@ public class MainController {
                 model.addAttribute("title","EBAtecPMS");
                 return "index";
             }
-            /**
-            if (form.getPassword1().equals(form.getPassword2())){//入力されたpasswordが一致した
-                try {
-                    UserDetails s = userDetailsService.loadUserByUsername(form.getUsername());
-
-                    model.addAttribute("title","会員登録 | EBAtecPMS");
-                    model.addAttribute("result","このユーザーは既に登録されております");
-                    model.addAttribute("username",form.getUsername());
-                    //model.addAttribute("email",email);
-                    return "register";
-
-                }catch (UsernameNotFoundException e){
-                    userAction.createUser(form.getUsername(), form.getPassword1());
-
-                    model.addAttribute("result","会員登録に成功しました！");
-                    model.addAttribute("title","EBAtecPMS");
-                    return "index";
-                }
-            }else{
-                model.addAttribute("title","会員登録 | EBAtecPMS");
-                model.addAttribute("result","入力されたパスワードが一致しておりません");
-                model.addAttribute("username",form.getPassword1());
-                //model.addAttribute("email",email);
-                return "register";
-            } **/
         }
     }
 }
