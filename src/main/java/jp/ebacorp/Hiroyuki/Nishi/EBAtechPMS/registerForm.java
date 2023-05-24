8db885af26ec812a535f08309be9f344653f6251
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-05-24 19:22:06 +0900

    単項目バリデーションエラーメッセージを修正

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAtechPMS/registerForm.java
@@ -1,18 +1,16 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAtechPMS;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 public class registerForm {
 
-    @Pattern(regexp = "[0-9a-zA-Z]{1,32}", message = "ユーザー名には１文字以上３２文字以内の英数字のみを使用してください")
+    @Pattern(regexp = "[0-9a-zA-Z]{1,32}")
     private String username;
 
-    @Pattern(regexp = "(?=.*[A-Z])(?=.*[.?/-])(?=.*[a-z])[a-zA-Z0-9.?/-]{8,24}", message = "passwordには８文字以上２４文字以内の英数字と記号のみを使用してください")
+    @Pattern(regexp = "(?=.*[A-Z])(?=.*[.?/-])(?=.*[a-z])[a-zA-Z0-9.?/-]{8,24}")
     private String password1;
 
     private String password2;
