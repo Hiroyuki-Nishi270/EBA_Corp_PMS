b2f460295cef573dda12f313683c0b8aa57aae77
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-06-20 16:43:12 +0900

    タスク用のフォームクラス修正

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAcorpPMS/TaskForm.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAcorpPMS/TaskForm.java
@@ -1,28 +1,49 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Calendar;
 import java.util.Date;
 
-@Data
 @Entity
+@Table(name = "Task_Data")
+@Data
 public class TaskForm {
-
+    //フィールド
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    //概要
-    @Length(min = 1, max = 255)
-    private String overview;
+    //start: '2018-10-01 0:00:00', //タスクの開始(時間0:00:00はデフォルトで入れなくてはいけない)
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Calendar start;
 
-    //期限
-    //@DateTimeFormat(pattern = "yyyy-MM-dd")
-    //private Date deadline;
+    //end: '2018-10-08 23:59:59', //タスクの終了(時間23:59:59はデフォルトで入れなくてはいけない)
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    private Calendar end;
+
+    //name: 'ウェブのリデザイン',    //タスク名
+    @Length(min = 1, max = 255)
+    @NotBlank
+    private String name;
+
+    //progress: 20,				//タスクの進捗(0~100)
+    @Min(0)
+    @Max(100)
+    private Integer progress;
+
+    //dependencies: 'Task 0'      //親タスクのid(','で区切る)
+    @Pattern(regexp = "[0-9,]")
+    private String dependencies;
+
+    @Length(min=0, max = 65535)
+    private String detail;
 
 }
