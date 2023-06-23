afdd9655a3c407f98ad914eefa61afc2047d5cb3
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-06-23 23:04:05 +0900

    不要なコードを削除

--- src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAcorpPMS/MainController.java
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAcorpPMS/MainController.java
@@ -1,20 +1,12 @@
 package jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS;
 
-import jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS.User.SignupForm;
+
 import jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS.task.CRUDTaskFormRepository;
 import jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS.task.TaskForm;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import javax.sql.DataSource;
 import java.util.List;
 
 @Controller
