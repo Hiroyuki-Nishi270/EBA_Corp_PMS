171954c5dce8aa9ec721fd2d33f325d5da6ea640
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-07-01 00:20:26 +0900

    タスクの期間変更時に、後続タスクも変更される機能を追加

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,24 @@
+function formatDate(dt) {
+    var y = dt.getFullYear();
+    var m = ('00' + (dt.getMonth() + 1)).slice(-2);
+    var d = ('00' + dt.getDate()).slice(-2);
+    return (y + '-' + m + '-' + d);
+}
+
+function readTaskListAsJSON() {
+    return JSON.parse(document.querySelector('#updated_task_list_by_gantt').value);
+}
+
+function overWrite(task) {
+    let taskList = readTaskListAsJSON();
+    taskList.forEach(function (value, index, array) {
+
+        if (value.id == task.id) {
+            taskList[index].start = task.start;
+            taskList[index].end = task.end;
+        }
+
+    });
+
+    document.querySelector('#updated_task_list_by_gantt').value = JSON.stringify(taskList);
+}
