e862a9e932879395bc3365cfd35a38c5cecc7171
Hiroyuki-Nishi270 <nishi2015_1107@yahoo.co.jp>
2023-07-26 16:32:34 +0900

    Add a feature which checks whether an input username by a new user has been registered or not.

---
+++ src/main/java/jp/ebacorp/Hiroyuki/Nishi/EBAcorpPMS/user/UserRestController.java
@@ -0,0 +1,32 @@
+package jp.ebacorp.Hiroyuki.Nishi.EBAcorpPMS.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+
+@RestController
+@RequestMapping("/")
+public class UserRestController {
+    @Autowired
+    DataSource dataSource;
+
+    @GetMapping("/checkUserName")
+    public int getCheckUserName(@RequestParam("username") String username){
+        ///System.out.println(username);
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        if(users.userExists(username)){
+            //System.out.println("1");
+            return 1;
+        }else{
+            //System.out.println("0");
+            return 0;
+        }
+
+    }
+}
